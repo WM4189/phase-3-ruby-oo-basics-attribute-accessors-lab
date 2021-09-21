b5156bcb136998816ba128823ead84bfbd3ba314
William Mosto <will.mosto@gmail.com>
2021-09-21 10:52:38 -0400

    commit tree 5fdb72bd5dedaf03840b38c6dd6228ab6cf217ea

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,12 @@
-## code your solution here. 
+class Cat
+    attr_accessor :name
+
+    def meow
+        puts "meow!"
+    end
+end
+
+pussums = Cat.new
+pussums.name = "Ludwig"
+puts pussums.meow
+# ruby lib/meowing_cat.rb
